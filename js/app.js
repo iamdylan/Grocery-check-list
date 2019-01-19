d1ac17305ff05f5b070e989507978fb18e301cb9
iamdylan <43369528+iamdylan@users.noreply.github.com>
2019-01-19 20:02:54 +0530

    Added application logic for warning messages

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const myApp = angular.module('myApp', []);
 
-myApp.factory = ('helperFactory', function() {
+myApp.constant('MAX_LENGTH', 50);
+myApp.constant('MIN_LENGTH', 2);
+
+myApp.factory('helperFactory', function() {
     return {
         filterFieldArrayByDone : function(thisArray, thisField, thisValue) {
             var arrayToReturn = [];
@@ -12,9 +15,10 @@ myApp.factory = ('helperFactory', function() {
             return arrayToReturn;
         }
     };
-});
+})
 
-myApp.controller('grocListController', ['$scope', function($scope) {
+myApp.controller('grocListController', ['$scope', '$http', '$log', 'helperFactory', 'MAX_LENGTH', 'MIN_LENGTH', 
+    function($scope, $http, $log, helperFactory, MAX_LENGTH, MIN_LENGTH) {
 
     var urlInsert = '/mod/insert.php';
     var urlSelect = '/mod/select.php';
@@ -27,5 +31,46 @@ myApp.controller('grocListController', ['$scope', function($scope) {
     $scope.item = '';
     $scope.qty = '';
     $scope.types = '';
+
+    $scope.charactersNeeded = function() {
+        var characters = (MIN_LENGTH - $scope.item.length);
+        return (characters > 0) ? characters : 0;
+    };
+
+    $scope.charactersRemaining = function() {
+        var characters = (MAX_LENGTH - $scope.item.length);
+        return (characters > 0) ? characters : 0;
+    };
+
+
+    $scope.charactersOver = function() {
+        var characters = (MAX_LENGTH - $scope.item.length);
+        return (characters < 0) ? Math.abs(characters) : 0;
+    };
+
+    $scope.minCharactersMet = function() {
+        return ($scope.charactersNeeded() == 0);
+    };
+
+    $scope.anyCharactersOver = function() {
+        return ($scope.charactersOver() > 0);
+    };
+
+    $scope.isCharWithinRange = function() {
+        return (
+            $scope.minCharactersMet() && !$scope.anyCharactersOver()
+        );
+    };
+
+    $scope.goodToGo = function() {
+        return (
+            $scope.isCharWithinRange() && $scope.qty > 0 && $scope.type > 0
+        );
+    };
+
+    $scope.clear = function() {
+        $scope.item = '';
+        $scope.qty = '';
+    };
 }]);
 
